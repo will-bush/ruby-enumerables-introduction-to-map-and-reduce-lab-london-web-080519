788c4f0ffc4b95a818de9ac0077b73fb5f51172d
Will Bush <williamdcbush@gmail.com>
2019-07-19 08:43:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,6 +50,12 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
 end
 
 def reduce_to_any_true(source_array)
